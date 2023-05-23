13f372546ebe31c537b5dd7add6512dcddb91938
Tiago Gomes <tiagogomes187@gmail.com>
2023-05-23 20:48:23 -0300

    Criando as camadas MVC

---
+++ src/test/java/br/dev/tiagogomes/eventoapp/EventoappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.tiagogomes.eventoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
